f19db16b0abaea0c0f5a8b194067508ff3c48dcd
Mohanad Inairat <mohannad.inairat@iqvia.com>
2023-11-27 20:36:00 +0200

    fix mobile for product

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,6 +1,37 @@
 // components/Tabs.js
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState<{ width: any; height: any }>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
 
 const Tabs = ({
   tabs,
@@ -12,19 +43,24 @@ const Tabs = ({
   onTabClick: any;
 }) => {
   const [firstVisibleTab, setFirstVisibleTab] = useState(0);
-
-  const showArrows = tabs.length > 4; // Show arrows if there are more than 4 tabs
-
+  const size = useWindowSize();
   const getTabsToShow = () => {
-    const lastVisibleTab = firstVisibleTab + 3; // Show up to 4 tabs
-    return tabs.slice(firstVisibleTab, lastVisibleTab + 1);
+    if (size.width >= 1024) {
+      return tabs.slice(firstVisibleTab, firstVisibleTab + 4);
+    } else if (size.width >= 600) {
+      return tabs.slice(firstVisibleTab, firstVisibleTab + 2);
+    } else {
+      return tabs.slice(firstVisibleTab, firstVisibleTab + 1);
+    }
   };
 
   const handleArrowClick = (direction: any) => {
+    const step = size.width >= 1024 ? 4 : size.width >= 600 ? 2 : 1;
+
     if (direction === "left") {
-      setFirstVisibleTab((prev) => Math.max(prev - 1, 0));
+      setFirstVisibleTab((prev) => Math.max(prev - step, 0));
     } else if (direction === "right") {
-      setFirstVisibleTab((prev) => Math.min(prev + 1, tabs.length - 4));
+      setFirstVisibleTab((prev) => Math.min(prev + step, tabs.length - step));
     }
   };
 
@@ -39,47 +75,36 @@ const Tabs = ({
         boxShadow: "0px 0px 2px 0px rgba(0, 0, 0, 0.25)",
         zIndex: 1,
         overflowX: "auto", // Enable horizontal scrolling on small screens
+        whiteSpace: "nowrap", // Prevent tabs from wrapping to the next line
       }}
     >
-      {showArrows && (
-        <button
-          onClick={() => handleArrowClick("left")}
-          disabled={firstVisibleTab === 0}
-        >
-          <Image
-            src="/arrow-left.svg"
-            width={30}
-            height={30}
-            alt="arrow-left"
-          />
-        </button>
-      )}
+      <button
+        onClick={() => handleArrowClick("left")}
+        disabled={firstVisibleTab === 0}
+      >
+        <Image src="/arrow-left.svg" width={30} height={30} alt="arrow-left" />
+      </button>
       {getTabsToShow().map((tab: any, index: any) => (
         <div
           key={index}
           className={`sm:px-6 py-3 justify-center text-center sm:justify-start ${
-            index === selectedTab ? "text-white" : "text-gray-100"
+            firstVisibleTab + index === selectedTab
+              ? "text-white"
+              : "text-gray-100"
           } inline-flex items-center leading-none tracking-wider`}
-          onClick={() => onTabClick(index)}
+          onClick={() => onTabClick(firstVisibleTab + index)}
         >
           <span className="title-font cursor-pointer font-normal text-[13px]">
             {tab}
           </span>
         </div>
       ))}
-      {showArrows && (
-        <button
-          onClick={() => handleArrowClick("right")}
-          disabled={firstVisibleTab === tabs.length - 4}
-        >
-          <Image
-            src="/arrow-right.svg"
-            width={30}
-            height={30}
-            alt="arrow-left"
-          />
-        </button>
-      )}
+      <button
+        onClick={() => handleArrowClick("right")}
+        disabled={firstVisibleTab === tabs.length - getTabsToShow().length}
+      >
+        <Image src="/arrow-right.svg" width={30} height={30} alt="arrow-left" />
+      </button>
     </div>
   );
 };
