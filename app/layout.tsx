a8bc499608beb2da39d11b1660c5ea46a854f826
Mohanad Inairat <mohannad.inairat@iqvia.com>
2024-01-13 19:34:22 +0200

    fix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { Metadata } from "next";
 import NavBar from "@/components/NavBar";
 
@@ -8,16 +6,16 @@ import Image from "next/image";
 
 import "./globals.css";
 
-const metadata: Metadata = {
-  title: "ARA",
-  description: "ARA",
+export const metadata: Metadata = {
+  title: "ARA Consulting",
+  description: "Take your business to new heights with SAP Business One",
 };
 
-export default function RootLayout({
-  children,
-}: {
+interface Props {
   children: React.ReactNode;
-}) {
+}
+
+export default function Layout({ children }: Props) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
@@ -25,49 +23,13 @@ export default function RootLayout({
           <header className="w-full absolute top-5 z-20">
             <NavBar />
           </header>
-          <main className="grow gap-2">
-            {children}
-          </main>
+          <main className="grow gap-2">{children}</main>
           <footer
             className="py-3 text-center text-xs"
             style={{ background: "#005375" }}
           >
             <Footer />
           </footer>
-          <div className="fixed bottom-10 right-10 flex flex-col">
-            <button
-              className="  text-white font-bold p-2 rounded-full shadow-lg"
-              style={{
-                padding: "15px",
-                background: "#FFFFFFCC",
-              }}
-            >
-              <Image
-                src="/whatsapp_floating.svg"
-                alt="whatsapp_floating"
-                width={33}
-                height={33}
-                className="w-[33px] h-[33px]"
-              />
-            </button>
-            <button
-              className="  text-white font-bold rounded-full shadow-lg"
-              style={{
-                background: "#FFFFFFCC",
-                padding: "17px",
-                marginTop: "5px",
-              }}
-              onClick={() => window.scrollTo(0, 0)}
-            >
-              <Image
-                src="/floating_up.svg"
-                alt="floating_up"
-                width={27}
-                height={27}
-                className="m-auto w-[27px] h-[27px]"
-              />
-            </button>
-          </div>
         </>
       </body>
     </html>
