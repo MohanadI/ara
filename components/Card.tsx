2552ebc43d34c1466d229e1acf05d9c59c5a807d
Mohanad Inairat <mohannad.inairat@iqvia.com>
2024-03-03 23:36:09 +0200

    add blog

--- components/Card.tsx
+++ components/Card.tsx
@@ -16,24 +16,25 @@ function CardComponent({
   publishedAt: string;
 }) {
   return (
-    <Card
-      className="max-w-sm"
-      imgAlt="Meaningful alt text for an image that is not purely decorative"
-      imgSrc={image}
-    >
-      <div className="flex items-center justify-between">
-        <p className="font-normal text-gray-600">{sub_title}</p>
-        <p className="flex font-normal text-[13px] text-gray-600">
-          <HiOutlineCalendar className="h-4 w-4 mr-1" />
-          {publishedAt}
-        </p>
-      </div>
-      <Link href={`/blogs/${slug}`}>
+    <Link href={`/blogs/${slug}`}>
+      <Card
+        className="max-w-sm"
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc={image}
+      >
+        <div className="flex items-center justify-between">
+          <p className="font-normal text-gray-600">{sub_title}</p>
+          <p className="flex font-normal text-[13px] text-gray-600">
+            <HiOutlineCalendar className="h-4 w-4 mr-1" />
+            {publishedAt}
+          </p>
+        </div>
+
         <h5 className="text-2xl font-bold tracking-tight text-gray-900">
           {title}
         </h5>
-      </Link>
-    </Card>
+      </Card>
+    </Link>
   );
 }
 
